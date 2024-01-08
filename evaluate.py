8752a7b4ccab75f0c3688f04c22dfd6cb3fdc71f
saegsali <joel@asper.ch>
2024-01-08 16:13:13 +0100

    Add option for AQ

--- evaluate.py
+++ evaluate.py
@@ -2,13 +2,17 @@ import click
 from evaluation.evaluate import run_all_scenarios_for_method
 
 from uncertainty.benchmark import Uncertainty
+from adversarial_querying.aq_baseline import AQBaseline
 
 @click.command()
 @click.option('--name', help='The name of the model/method you want to test.')
-def run(name):
+@click.option('--add_noise', default=False, help='Whether to add noise to the AQ model or not.')
+def run(name, add_noise):
     """ Runs all tests for the specified model/method and reports the results """
     if name == "AQ":
-        pass
+        # for practical reasons you can choose this smaller than 1000 because it takes ages to run it even on a GPU
+        # will reduce the statistical significance though
+        run_all_scenarios_for_method(AQBaseline(add_noise=add_noise), num_tasks=1000)
     elif name == "uncertainty":
         # for practical reasons you can choose this smaller than 1000 because it takes ages to run it even on a GPU
         # will reduce the statistical significance though
